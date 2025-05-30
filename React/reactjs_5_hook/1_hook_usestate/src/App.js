e3bf1511e2ba7904cbc3b7b4d6bc47640d3ab2d5
yousef sharbi <youseftareq33@gmail.com>
2025-05-30 10:28:29 +0300

    Version 1

---
+++ React/reactjs_5_hook/1_hook_usestate/src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+
+import { useState } from 'react';
+
+function App() {
+
+  // without useState
+  let init_name="Yousef";
+
+  // with useState
+  const[name,setName]=useState("Yousef");
+  // name: is the value that assign using use-state by setName
+  // setName: function to change state (change text) of name
+  // useState: has the current state
+
+  const[count,setCount]=useState(0);
+
+  return (
+    <div className="App">
+
+      <h1>Without useState</h1>
+      <input type='text' placeholder='Enter Name' value={init_name}></input>
+      <p>Hello, {init_name}</p>
+
+      <br></br><br></br>
+
+      <h1>With useState</h1>
+      <input type='text' placeholder='Enter Name' value={name} onChange={(e)=>setName(e.target.value)}></input>
+      <p>Hello, {name}</p>
+
+      <br></br><br></br>
+      <p>u clicked {count} times</p>
+      <button onClick={()=>setCount(count+1)}>Click Me</button>
+      
+    </div>
+  );
+}
+
+export default App;
