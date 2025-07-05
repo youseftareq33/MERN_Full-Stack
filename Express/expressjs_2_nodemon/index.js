3e8dddcc326ed2b0044d7fcd766e8630fffd8eb4
yousef sharbi <youseftareq33@gmail.com>
2025-07-05 15:28:27 +0300

    Version 1

---
+++ Express/expressjs_2_nodemon/index.js
@@ -0,0 +1,17 @@
+const express=require('express');
+
+
+// init app
+const app=express();
+
+// get request
+app.get('/hi',(request,response)=>{
+    response.send("Hi from nodemon");
+});
+
+app.listen(5000,console.log("Server is running on port 5000"));
+
+
+// inside package.json there is script, inside it we can create shourtcut of keywords
+// ex: when we run nodemon, we write "nodemon index", in script i create "start" which equal to "nodemon index"
+// which in terminal "npm start"==="nodemon index"
